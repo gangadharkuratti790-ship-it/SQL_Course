17f03a578864b01962784cf4144f0ca6f4b095ae
Gangadhar <gangadharkuratti790@gmail.com>
2026-07-16 21:11:28 +0530

    Added program

---
+++ Database_Queries.sql
@@ -0,0 +1,13 @@
+-- Snippet for the queries related to database
+
+create database database_name; -- Creation of database
+create database if not exists database_name; -- Creation of database if same database do not exist
+
+drop database database__name;  -- to drop database
+drop database if exists database__name;  -- to drop database if exists
+
+use database database__name;   -- Usage of database
+show database database__name;  -- Displays database
+
+
+
