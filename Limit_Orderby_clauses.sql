820698bacdc8ff04ce642361b4a2f077ad32cb03
Gangadhar <gangadharkuratti790@gmail.com>
2026-07-22 13:25:19 +0530

    added program

---
+++ Limit_Orderby_clauses.sql
@@ -0,0 +1,11 @@
+-- Limit clause
+-- Sets an upper limit on number of (tuples)rows to be returned
+SELECT *
+FROM table_name
+limit 3;
+
+-- Orderby clause
+-- To sort in ascending (ASC) or descending order (DESC)
+SELECT *
+FROM table_name
+Order by column_name ASC;
